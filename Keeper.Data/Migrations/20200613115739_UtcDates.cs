1046fd9cfe12224a7c1c05ca88c6dcfa3315fd1d
Francois Laubscher <francoislaubscher@outlook.com>
2020-06-13 14:20:33 +0200

    utc datetime migration

---
+++ Keeper.Data/Migrations/20200613115739_UtcDates.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Keeper.Data.Migrations
+{
+    public partial class UtcDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Modified",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Modified",
+                table: "ProjectTasks",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Date",
+                table: "ProjectTasks",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "ProjectTasks",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Modified",
+                table: "Projects",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Projects",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Modified",
+                table: "Contacts",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Contacts",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Modified",
+                table: "Clients",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Users",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "ProjectTasks",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "ProjectTasks",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "ProjectTasks",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Projects",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Contacts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Contacts",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Clients",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+        }
+    }
+}
